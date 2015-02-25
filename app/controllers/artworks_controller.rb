8a69914d44e4df8f52e03701a09db379360d8e33
jbmoutout <jbmoutout@gmail.com>
2015-02-25 10:57:50 +0100

    twiiiiiiiiiiiter

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,18 +1,6 @@
 
-
 class ArtworksController < ApplicationController
 
-
-  def tweet(tweet)
-    @twitter = Twitter::REST::Client.new do |config|
-      config.consumer_key = ENV['TWITTER_KEY']
-      config.consumer_secret = ENV['TWITTER_SECRET']
-      config.access_token = ENV['TWITTER_TOKEN']
-      config.access_token_secret = ENV['TWITTER_TOKEN_SECRET']
-    end
-
-    client.update(tweet)
-  end
 
   def new
 
@@ -31,7 +19,7 @@ class ArtworksController < ApplicationController
   end
 
   def index
-
+    @tweets = @client.user_timeline("nonprintingc")
   end
 
   def show
