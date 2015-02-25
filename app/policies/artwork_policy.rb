e8455116452797176c700f09ff0337e13fab9a2b
jbmoutout <jbmoutout@gmail.com>
2015-02-25 16:53:12 +0100

    Edward again

--- app/policies/artwork_policy.rb
+++ app/policies/artwork_policy.rb
@@ -9,13 +9,23 @@ class ArtworkPolicy < ApplicationPolicy
 
   class Scope < Scope
     def resolve
+      output = { status: [] }
       client = Twitter::REST::Client.new do |config|
         config.consumer_key = ENV['TWITTER_KEY']
         config.consumer_secret = ENV['TWITTER_SECRET']
         config.access_token = ENV['TWITTER_TOKEN']
         config.access_token_secret = ENV['TWITTER_TOKEN_SECRET']
       end
-      client.user_timeline("nonprintingc")
+      output[:timeline] = client.user_timeline("nonprintingc")
+      output[:timeline].each do |tweet|
+        unless tweet.in_reply_to_status_id.nil?
+          begin
+            output[:status] << client.status(tweet.in_reply_to_status_id)
+          rescue
+          end
+        end
+      end
+      output
     end
   end
 end
