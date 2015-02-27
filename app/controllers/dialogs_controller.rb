b99ef8c3e34ab9d8f927f271adfe95ff8aefa74d
jbmoutout <jbmoutout@gmail.com>
2015-02-27 16:15:30 +0100

    like function

--- app/controllers/dialogs_controller.rb
+++ app/controllers/dialogs_controller.rb
@@ -1,7 +1,23 @@
 class DialogsController < ApplicationController
-
+  skip_after_action :verify_authorized, only: :collection
   def index
     @dialogs = policy_scope(Dialog)
   end
 
+  def collection
+    @dialogs = current_user.find_voted_items
+  end
+
+  def add
+    set_dialog
+    @dialog.liked_by current_user
+    redirect_to collection_path
+    authorize @dialog
+  end
+
+  private
+
+  def set_dialog
+    @dialog = Dialog.find(params[:dialog_id])
+  end
 end
