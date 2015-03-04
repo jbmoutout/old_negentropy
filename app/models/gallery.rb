e8b6edc053b348ecb6f39d5a9564107ebd2a80f0
jbmoutout <jbmoutout@gmail.com>
2015-03-04 13:20:09 +0100

    save Kimono to Gallery

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -4,19 +4,30 @@ class Gallery < ActiveRecord::Base
   has_many :artists
 
 
-  def self.save_gallery
-
+  def self.import
     response = RestClient.get 'https://www.kimonolabs.com/api/dimi5wmu?apikey=u6sMzW1WypgPUjpDTCTuVrAaqqPpomOZ'
 
-    r = JSON.parse(response.body)
+    response_hash = JSON.parse(response.body)
 
-          artist_name = r["results"]["collection1"][0]["artist_list"]["text"]
-          artist = Artist.where(name: artist_name).first_or_create!
+    gallery_name  = response_hash['name']
+    gallery       = Gallery.where(name: gallery_name).first_or_create!
 
-          gallery_name = r["name"]
-          gallery = Gallery.where(name: gallery_name).first_or_create!
+    response_hash['results']['collection1'].each do |artist|
+      artist_name = artist['artist_list']['text']
+      artist      = Artist.where(name: artist_name).first_or_create!
 
-    artist.galleries << gallery
+      unless gallery.artists.where(id: artist.id).exist?
+        gallery.artists << artist
+      end
+    end
+
+          # artist_name = r["results"]["collection1"][0]["artist_list"]["text"]
+          # artist = Artist.where(name: artist_name).first_or_create!
+
+          # gallery_name = r["name"]
+          # gallery = Gallery.where(name: gallery_name).first_or_create!
+
+    # artist.galleries << gallery
 
   end
 
