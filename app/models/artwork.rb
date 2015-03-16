ea0dd1dc5fe9e92dc8ae265eac0fca27515e50a7
jbmoutout <jbmoutout@gmail.com>
2015-03-16 14:50:50 +0100

    working on artworks

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,3 +1,5 @@
+require 'rest_client'
+
 class Artwork < ActiveRecord::Base
   belongs_to :artist
 
@@ -42,6 +44,33 @@ class Artwork < ActiveRecord::Base
       artwork
     end
   end
+
+
+    #TODO:
+
+   def self.import(uri)
+      response = RestClient.get (uri)
+
+      response_hash = JSON.parse(response.body)
+
+      gallery_name  = response_hash['name']
+      gallery       = Gallery.where(name: gallery_name).first_or_create!
+
+      response_hash['results']['collection1'].each do |artist|
+
+        artist_name = artist['property1']['text']
+
+        temp = artist_name.dup
+        artist_name.clear
+        artist_name << temp.split.map(&:capitalize!).join(' ')
+
+        artist      = Artist.where(name: artist_name).first_or_create!
+
+        unless gallery.artists.where(id: artist.id).exists?
+          gallery.artists << artist
+        end
+      end
+    end
 
 
 
