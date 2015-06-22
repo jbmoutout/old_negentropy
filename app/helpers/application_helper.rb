4a01393e09b9e83c51834c401180e259b0a52121
jbmoutout <jbmoutout@gmail.com>
2015-06-22 23:46:40 +0200

    instagram API + save

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
 
-  def get_related_url(dialog_id)
+
+def get_related_url(dialog_id)
     dialog = Dialog.find(dialog_id)
     artist = dialog.artwork.artist
     related_artists = []
@@ -29,5 +30,44 @@ module ApplicationHelper
      return artworks_links[0..6]
 
   end
+
+
+
+
+
+
+
+#####
+#old Twitter get_related for Dialog
+
+  # def get_related_url(dialog_id)
+  #   dialog = Dialog.find(dialog_id)
+  #   artist = dialog.artwork.artist
+  #   related_artists = []
+  #   related_artworks = []
+  #   artworks_links = []
+
+  #   if artist.galleries.any?
+  #     galleries = artist.galleries
+  #      galleries.each do |gallery|
+  #        related_artists += gallery.artists.joins(:galleries).distinct
+  #      end
+
+  #      related_artists.uniq!
+  #    end
+
+  #   related_artists.each do |artist|
+  #     related_artworks += artist.artworks
+  #   end
+
+  #   related_artworks.each do |artwork|
+  #     if artwork.dialogs[0]
+  #        artworks_links += artwork.dialogs
+  #     end
+  #   end
+
+  #    return artworks_links[0..6]
+
+  # end
 
 end
